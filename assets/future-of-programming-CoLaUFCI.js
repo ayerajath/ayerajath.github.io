4693d4d633055d781b8e704dad949a5870cda283
ayerajath <ayerajath@users.noreply.github.com>
2025-07-11 14:19:26 +0000

    deploy: e375f7c76a5f98f441d69b31efe2175994bbf29a

---
+++ assets/future-of-programming-CoLaUFCI.js
@@ -0,0 +1,25 @@
+const e=`---
+title: Future of Programming
+date: 2024-08-24 01:00:00
+slug: future-of-programming
+tags: [tech]
+draft: false
+---
+
+With the introduction of tools like Cursor. And the LLMs(especially Claude) getting better at programming and coding. These definitely look like the future. We can only expect these to get better, even if marginally. 
+
+The rate at which we can ship stuff is faster than ever. 
+
+I was able to get this blog up and running in a matter of hours. Earlier, I would have spent days, getting this done.
+
+
+The next few years, there will be more and more tools like these and each one of these will probably be better than the last. The rate at which applications and softwares will be shipped or created will be on a rise. 
+
+How can one stand out? Even I don't know, maybe we'll find out.
+
+What will I be doing? Learn more, Experiment more. Build more. Put a name out there.
+
+A lot of roles will probably still continue. One being an cracked product engineer. Someone who understands not just engineering, but also other aspects to a product, like marketing, sales, etc. The other being a core low level engineer, the ones that are able to do engineering at the lowest level. 
+
+This is the way building software is headed. A worker always in the IDE for a programmer. Let's see where this goes.
+`;export{e as default};
