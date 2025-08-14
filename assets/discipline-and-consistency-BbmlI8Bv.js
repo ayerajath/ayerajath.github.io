0080af668541a92d46df7ca9c9c109d5274b3291
ayerajath <ayerajath@users.noreply.github.com>
2025-08-14 17:56:28 +0000

    deploy: 84588704421b36c9267263b07eb1cc913766c130

---
+++ assets/discipline-and-consistency-BbmlI8Bv.js
@@ -0,0 +1,21 @@
+const e=`---
+title: A Little Better Every Day
+date: 2025-08-09 01:00:00
+slug: discipline-and-consistency
+tags: [random]
+draft: false
+---
+
+I was staring at the Strava app. It’s just a tracker for my runs (yes, it does more), but for me, the reason I keep coming back is the analytics.
+
+It made me think — gamifying anything works. Add a sense of progress, competition, and suddenly it sticks. Duolingo gets this so right. The way they’ve gamified learning is beautiful. I find myself opening the app just to keep a streak going.
+
+Underneath all of it, it’s the same thing: consistency. And behind that? Discipline.
+
+I like being in office by 10:00 AM. Doesn’t matter that I’m at a startup — this small rule helps me get a head start on my day. I wake up at 6:00 AM most days. When I don’t, it feels off. I take pride in that routine.
+
+I want to be consistent. And the only way to do that is through discipline. Do the same thing every day. Try it for a week. See what happens. I want to gamify the things I care about. Pick something, do it every day — even if it’s just for 10 minutes. That’s how I hope to become a better software engineer. A better person.
+
+Just a slightly better version of myself than yesterday. Even something as small as reading a book for 15 minutes — it only works if I make the space to actually do it.
+
+**_Turns out, showing up — even for 10 minutes — is the real flex._**`;export{e as default};
